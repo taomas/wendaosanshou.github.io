2651fcf8efc94726f49f56c9c6178f58f9702039
wendaosanshou <947809647@qq.com>
2019-01-16 23:14:58 +0800

    feat: 压缩图片&完成所有切图细节

--- pengcheng/js/swiper.js
+++ pengcheng/js/swiper.js
@@ -1,5 +1,13 @@
 ;(function() {
-  window.onload = function() {
+  function onNavToogle() {
+    document
+      .querySelector('.js-planting')
+      .addEventListener('click', function() {
+        location.href = 'https://www.baidu.com'
+      })
+  }
+
+  function initSwiper() {
     var mySwiper = new Swiper('.swiper-container', {
       direction: 'horizontal', // 垂直切换选项
       loop: true, // 循环模式选项
@@ -8,5 +16,9 @@
         el: '.swiper-pagination'
       }
     })
+  }
+  window.onload = function() {
+    initSwiper()
+    // onNavToogle()
   }
 })()
