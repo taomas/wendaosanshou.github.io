1ef679c9f3b6ddb0d1e6bee1b2cf3518fef34e5c
wendaosanshou <947809647@qq.com>
2019-01-18 22:07:12 +0800

    feat：完成信息流页面

---
+++ pengcheng/js/correction-flow.js
@@ -0,0 +1,41 @@
+;(function() {
+  function initItemClick() {
+    $('.info-answer-item').on('click', function() {
+      $('.info-answer-item').removeClass('active')
+      $(this).addClass('active')
+    })
+  }
+
+  let timer = ''
+  function initScroll() {
+    let scrollDom = $('.tel-list-content')
+    clearTimeout(timer)
+    timer = setInterval(function() {
+      autoScroll(scrollDom)
+    }, 2000)
+  }
+
+  function autoScroll(obj) {
+    var $self = obj.find('ul')
+    var lineHeight = $self.find('li:first').height()
+    $self.animate(
+      {
+        marginTop: -lineHeight + 'px'
+      },
+      600,
+      function() {
+        $self
+          .css({
+            marginTop: 0
+          })
+          .find('li:first')
+          .appendTo($self)
+      }
+    )
+  }
+
+  $(document).ready(function() {
+    initItemClick()
+    initScroll()
+  })
+})()
